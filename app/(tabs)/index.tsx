8399ecd703f3b785d978491be5fac2297b9e20cf
Surfsol <surfsol@protonmail.com>
2024-10-23 14:09:23 -0300

    add button speech

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet } from 'react-native';
-
+import { Button, StyleSheet } from 'react-native';
+import * as Speech from 'expo-speech';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
-export default function TabOneScreen() {
+export default function ButtonSpeech() {
+  const speak = () => {
+    const thingToSay = 'Bem Vindo Mio';
+    Speech.speak(thingToSay);
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Press to Speak</Text>
+
+      <Button title='Press to hear some words' onPress={speak} />
+
+      <EditScreenInfo path='app/(tabs)/index.tsx' />
     </View>
   );
 }
@@ -18,6 +24,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
   title: {
     fontSize: 20,
