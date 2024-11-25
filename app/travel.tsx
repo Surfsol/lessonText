5dcaa86f9e0fc6f2c5946d835f6f769a1647172f
Surfsol <surfsol@protonmail.com>
2024-11-25 08:39:16 -0300

    add video play

--- app/travel.tsx
+++ app/travel.tsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Pressable } from 'react-native';
 import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
-
+import { useRouter } from 'expo-router';
 import { KeyboardAvoidingView, Platform } from 'react-native';
+
 
 interface TravelProps {
   userLogin: string | undefined;
@@ -22,13 +19,13 @@ const countryList = [
   { id: '4', title: 'France' },
   { id: '5', title: 'Germany' },
 ];
-const cityList: { [key: string]: { id: string; title: string }[] }  = {
+const cityList: { [key: string]: { id: string; title: string }[] } = {
   'United States': [
     { id: '1', title: 'New York' },
     { id: '2', title: 'Los Angeles' },
     { id: '3', title: 'Chicago' },
   ],
-  'Canada': [
+  Canada: [
     { id: '1', title: 'Toronto' },
     { id: '2', title: 'Vancouver' },
     { id: '3', title: 'Montreal' },
@@ -56,70 +53,78 @@ const Travel: React.FC<TravelProps> = ({ userLogin }) => {
     city: null,
     purpose: null,
   });
-  const [filteredCities, setFilteredCities] = useState<{ id: string; title: string }[] | []>([]);
-
+  const [filteredCities, setFilteredCities] = useState<
+    { id: string; title: string }[] | []
+  >([]);
+  const router = useRouter();
   const handleCountrySelect = (item: AutocompleteDropdownItem | null) => {
     if (item?.title) {
-      for (let i = 0; i < countryList.length - 1; i++){
-        if(countryList[i].title === item.title){
-          setTravel({...travel, country: item.title})
+      for (let i = 0; i < countryList.length - 1; i++) {
+        if (countryList[i].title === item.title) {
+          setTravel({ ...travel, country: item.title });
           setFilteredCities(cityList[item.title]);
         }
       }
-     
-    } 
+    }
   };
 
   const handleCitySelect = (item: AutocompleteDropdownItem | null) => {
     if (item?.title) {
-      setTravel({...travel, city : item.title})
+      setTravel({ ...travel, city: item.title });
     }
-  }
+  };
 
   return (
     <View style={styles.card}>
       <Text style={styles.label}>
         Select the Country and City you want to visit:
       </Text>
+
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={{ flex: 1 }}
       >
-        <View style={styles.countryCity}>
-          <AutocompleteDropdown
-            clearOnFocus={false}
-            closeOnBlur={true}
-            closeOnSubmit={false}
-            dataSet={countryList}
-            onSelectItem={handleCountrySelect}
-            textInputProps={{
-              placeholder: 'Country',
-              autoCorrect: false,
-              style: {
-                borderWidth: 1,
-                borderColor: '#ccc',
-                padding: 10,
-                color: 'blue',
-              },
-            }}
-            containerStyle={styles.autocompleteContainer}
-          />
-          {filteredCities.length > 0 && (
+        <View style={{ zIndex: 1 }}>
+          {/* Ensure dropdowns are on top */}
+          <View style={styles.countryCity}>
             <AutocompleteDropdown
               clearOnFocus={false}
-              closeOnBlur={false}
+              closeOnBlur={true}
               closeOnSubmit={false}
-              initialValue={{ id: '1' }} // optional
-              dataSet={filteredCities}
-              onSelectItem={handleCitySelect}
+              dataSet={countryList}
+              onSelectItem={handleCountrySelect}
               textInputProps={{
+                placeholder: 'Country',
                 autoCorrect: false,
-                style: styles.input,
+                style: {
+                  borderWidth: 1,
+                  borderColor: '#ccc',
+                  padding: 10,
+                  color: 'blue',
+                },
               }}
               containerStyle={styles.autocompleteContainer}
-              inputContainerStyle={styles.inputContainer}
             />
-          )}
+            {filteredCities.length > 0 && (
+              <AutocompleteDropdown
+                clearOnFocus={false}
+                closeOnBlur={false}
+                closeOnSubmit={false}
+                initialValue={{ id: '1' }} // optional
+                dataSet={filteredCities}
+                onSelectItem={handleCitySelect}
+                textInputProps={{
+                  autoCorrect: false,
+                  style: styles.input,
+                }}
+                containerStyle={styles.autocompleteContainer}
+                inputContainerStyle={styles.inputContainer}
+              />
+            )}
+          </View>
+          <TouchableOpacity style={styles.confirmButton} onPress={() => router.push('/playvideo')}>
+            <Text style={styles.confirmText}> Confirm</Text>
+          </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
     </View>
@@ -133,11 +138,25 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#f9f9f9',
   },
-  countryCity: { flex: 1, flexDirection: 'row', zIndex: 1 },
+  countryCity: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
   label: {
     fontSize: 16,
-    marginBottom: 10,
+    marginBottom: 1,
     color: '#333',
+  },
+  confirmButton: {
+    marginTop: 10, // Spacing from dropdowns
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  confirmText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
   input: {
     borderWidth: 1,
@@ -147,10 +166,8 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   autocompleteContainer: {
-    marginBottom: 20,
-    // borderWidth: 1,
-    // borderColor: '#ddd',
-    // borderRadius: 5,
+    flex: 1,
+    marginHorizontal: 5,
   },
   inputContainer: {
     borderWidth: 0,
